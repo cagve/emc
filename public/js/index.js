f968033daff78f10e498ea283cd4c9e433855c4c
Carlos AGUILERA VENTURA <caguiler@hal9000.irit.fr>
2023-09-21 13:52:56 +0200

    style square

--- public/js/index.js
+++ public/js/index.js
@@ -116,7 +116,7 @@ function create_graph(){
 		elements: [{data: { id: 'n1'}}],
 		style: [
 			{selector: 'node', style: {'shape': 'square'}},
-			{selector: '.valuation', style: {"color":"white", "font-family":"bold",  'background-color':'#183153', 'text-valign':'center', 'text-halign':'center', 'content': 'data(name)'} },
+			{selector: '.valuation', style: {'shape':'square', "color":"white", "font-family":"bold",  'background-color':'#183153', 'text-valign':'center', 'text-halign':'center', 'content': 'data(name)'} },
 			{selector: '.world', style: { 'background-color':'#183153',  'label':'data(id)', "text-background-opacity": 0.7, "color": "#183153", "text-background-color": "white"}},
 			{selector: '.relation', style: {'target-arrow-shape': 'triangle', 'arrow-scale':'2', 'label': 'data(name)', "text-background-opacity": 1, "color": "#183153", "text-background-color": "white" }}
 		]
